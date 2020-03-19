92140a56a798d8fce90adfd28215b5c854f683f7
Avraham Jacobsohn <noreallyimfine@users.noreply.github.com>
2020-03-19 11:25:11 -0400

    add line breaks for neater printing

--- prepare_and_label.py
+++ prepare_and_label.py
@@ -25,6 +25,8 @@ Stesp include:
 	- create regional rarity column
 	- create seasonal regional rarity column"""
 
+print("Reading in bird csv...")
+print()
 df = pd.read_csv('C:\\Users\\ajaco\\Downloads\\ebd_relJan-2020.txt',
 				 sep='\t', 
 				 nrows=200000, 
@@ -35,6 +37,8 @@ assert(df.shape == (200000, 8))
 
 #===============================
 
+print("Renaming columns...")
+print()
 df.rename(columns={
     'COMMON NAME': 'name',
     'OBSERVATION COUNT': 'observ_count',
@@ -50,17 +54,25 @@ assert('name' in df.columns)
 
 #=================================
 
+print("Filtering for birds seen in the United States...")
+print()
 us_birds = df.query("country == 'United States'")
 assert(us_birds.shape == (105294, 8))
 assert(len(us_birds['country'].unique() == 1))
 
 #==================================
 
+print("Dropping rows with null values for 'county' column")
+print("These are bad data...")
+print()
 us_birds.dropna(subset=['county'], inplace=True)
 assert(us_birds.isnull().sum()['county'] == 0)
 assert(us_birds.shape == (105077, 8))
+
 #==================================
 
+print('Replacing "X" in "Observation Count" with 1...')
+print()
 us_birds['observ_count'] = us_birds['observ_count'].apply(
 									lambda x: 1 if x == 'X' else x)
 
@@ -68,17 +80,23 @@ assert('X' not in us_birds.observ_count.unique())
 
 #===================================
 
+print("Converting 'Observation Date' to datetime object...")
+print()
 us_birds['observ_date'] = pd.to_datetime(us_birds['observ_date'], infer_datetime_format=True)
 assert(us_birds.observ_date.dtype == 'datetime64[ns]')
 
 #===================================
 
+print("Extracting month from 'Observation Date'...")
+print()
 us_birds['month'] = us_birds.observ_date.dt.month
 assert('month' in us_birds.columns)
 assert(us_birds.month.dtype == 'int64')
 
 #===================================
 
+print("Creating a season column from the month column...")
+print()
 def season_from_month(x):
 	if x in [12, 1, 2]:
 		return 'Winter'
@@ -94,11 +112,15 @@ assert(us_birds.season.value_counts()['Spring'] == 39610)
 
 #======================================
 
+print('Creating joint county and state column to merge on...')
+print()
 us_birds['county_state'] = us_birds['county'] + us_birds['state']
 assert(us_birds.shape == (105077, 11))
 
 #=======================================
 
+print("Reading in file with reions on it...")
+print()
 regions = pd.read_excel("C:\\Users\\ajaco\\Desktop\\repos\\noreallyimfine\\ebird-project\\URAmericaMapCountyList.xlsx",
 						skiprows=3,
 						usecols=['State', 'CountyName', 'RegionName'])
@@ -107,22 +129,30 @@ assert(regions.shape == (3142, 3))
 
 #======================================
 
+print("Stripping leading whitespace from State...")
+print()
 regions['State'] = regions['State'].str.strip()
-
 assert('California' in regions['State'].values)
 
 #======================================
 
+print("Dropping leading numbers from RegionName...")
+print()
 regions['RegionName'] = regions['RegionName'].apply(lambda x: ' '.join(x.split()[1:]))
 assert('Deep South' in regions['RegionName'].values)
 
 #======================================
 
+print("Dropping state from CountyName...")
+print()
 regions['CountyName'] = regions['CountyName'].apply(lambda x: x.split(',')[0])
 assert('Cook County' in regions['CountyName'].values)
 
 #======================================
 
+print("Mapping names of Alaska counties to match the way...")
+print("they're named in the bird df...")
+print()
 county_dict = {
     'Aleutians East Borough': 'Aleutians East',
     'Aleutians West Census Area': 'Aleutians West',
@@ -161,12 +191,15 @@ assert('North Slope Borough' not in regions['CountyName'].values)
 
 #===============================================
 
+print("Dropping 'Parish' from Louisiana counties...")
+print()
 regions['CountyName'] = regions['CountyName'].apply(lambda x: x if 'Parish' not in x else ' '.join(x.split()[:-1]))
-
 assert('West Baton Rouge' in regions['CountyName'].values)
 
 #================================================
 
+print("Capitalizing 'city' in Richmond and St. Louis county...")
+print()
 assert(regions.at[2944, 'CountyName'] == 'Richmond city')
 assert(regions.at[1597, 'CountyName'] == 'St. Louis city')
 regions.at[2944, 'CountyName'] = 'Richmond City'
@@ -174,17 +207,24 @@ regions.at[1597, 'CountyName'] = 'St. Louis City'
 
 #===============================================
 
+print("Dropping 'county' from the counties that have it...")
+print()
 regions['CountyName'] = regions['CountyName'].apply(lambda x: x if 'County' not in x else ' '.join(x.split()[:-1]))
 assert('Los Angeles' in regions['CountyName'].values)
 
 #===============================================
 
+print('Creating joint county and state column to merge on...')
+print()
 regions['county_state'] = regions['CountyName'] + regions['State']
 assert('AutaugaAlabama' in regions['county_state'].values)
 
 #===============================================
 
+print("Merging birds and regions...")
+print()
 merged = us_birds.merge(regions)
 assert(merged.shape == (104732, 14))
 
 #===============================================
+print("Data Cleaning and Preparation Complete.")
