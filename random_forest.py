9dfa21894b838e726619f3867a88ed2a868e2944
Avraham Jacobsohn <noreallyimfine@users.noreply.github.com>
2020-04-04 11:16:18 -0400

    complete read in df and X,y split steps

--- random_forest.py
+++ random_forest.py
@@ -2,3 +2,24 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 
 import category_encoders as ce
+import pandas as pd
+import numpy as np
+
+# ======================================
+
+print("Reading in labeled dataset sample...")
+print()
+
+df = pd.read_csv("C:\\Users\\ajaco\\Desktop\\repos\\noreallyimfine\\ebird-project\\labelled_bird_sample.csv")
+assert(df.shape == (103992, 16))
+
+# ========================================
+
+print("Select features and target to split dataframe into X and y...")
+print()
+
+features = ['name', 'season', 'RegionName', 'latitude', 'longitude']
+target = 'target'
+
+X = df[features]
+y = df[target]
