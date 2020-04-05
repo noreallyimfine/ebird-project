0eed89aa001161da6ed94fa60057456196543f0b
Avraham Jacobsohn <noreallyimfine@users.noreply.github.com>
2020-04-05 13:54:11 -0400

    correct issue reading in df - can't skip the header line

--- sample/model_test.py
+++ sample/model_test.py
@@ -3,12 +3,13 @@ import joblib
 
 
 # Get 50,000 new instances from dataset
-df = pd.read_csv("C:\\Users\\ajaco\\Desktop\\repos\\noreallyimfine\\ebird-project\\data\\ebd_relJan-2020.txt", 
-                  skiprows=200000,
-                  nrows=50000,
-                  usecols=['COMMON NAME', 'COUNTRY', 'STATE', 'COUNTY',
-                           'LATITUDE', 'LONGITUDE', 'OBSERVATION DATE',
-                           'OBSERVATION COUNT'])
+df = pd.read_csv("C:\\Users\\ajaco\\Desktop\\repos\\noreallyimfine\\ebird-project\\data\\ebd_relJan-2020.txt",
+                 sep='\t',
+                 skiprows=lambda x: x in range(1, 200001),
+                 nrows=50000,
+                 usecols=['COMMON NAME', 'COUNTRY', 'STATE', 'COUNTY',
+                          'LATITUDE', 'LONGITUDE', 'OBSERVATION DATE',
+                          'OBSERVATION COUNT'])
 
 assert(df.shape == (50000, 8))
 
