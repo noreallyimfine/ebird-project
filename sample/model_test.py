80b872d28ce5d056ac79e1802823e602dea078cd
Avraham Jacobsohn <noreallyimfine@users.noreply.github.com>
2020-04-05 14:42:06 -0400

    up to merge step

--- sample/model_test.py
+++ sample/model_test.py
@@ -74,3 +74,55 @@ regions = pd.read_excel("C:\\Users\\ajaco\\Desktop\\repos\\noreallyimfine\\ebird
                         skiprows=3,
                         usecols=['State', 'CountyName', 'RegionName'])
 
+
+regions['State'] = regions['State'].str.strip()
+regions['RegionName'] = regions['RegionName'].apply(lambda x: ' '.join(x.split()[1:]))
+regions['CountyName'] = regions['CountyName'].apply(lambda x: x.split(',')[0])
+
+county_dict = {
+    'Aleutians East Borough': 'Aleutians East',
+    'Aleutians West Census Area': 'Aleutians West',
+    'Anchorage Municipality': 'Anchorage',
+    'Bethel Census Area': 'Bethel',
+    'Bristol Bay Borough': 'Bristol Bay',
+    'Denali Borough': 'Denali',
+    'Dillingham Census Area': 'Dillingham',
+    'Fairbanks North Star Borough': 'Fairbanks North Star',
+    'Haines Borough': 'Haines',
+    'Hoonah-Angoon Census Area': 'Skagway-Hoonah-Angoon',
+    'Juneau City and Borough': 'Juneau',
+    'Kenai Peninsula Borough': 'Kenai Peninsula',
+    'Ketchikan Gateway Borough': 'Ketchikan Gateway',
+    'Kodiak Island Borough': 'Kodiak Island',
+    'Kusilvak Census Area': 'Kusilvak',
+    'Lake and Peninsula Borough': 'Lake and Peninsula',
+    'Matanuska-Susitna Borough': 'Matanuska-Susitna',
+    'Nome Census Area': 'Nome',
+    'North Slope Borough': 'North Slope',
+    'Northwest Arctic Borough': 'Northwest Arctic',
+    'Petersburg Borough': 'Petersburg Borough',
+    'Prince of Wales-Hyder Census Area': 'Prince of Wales-Outer Ketchikan',
+    'Sitka City and Borough': 'Sitka',
+    'Southeast Fairbanks Census Area': 'Southeast Fairbanks',
+    'Valdez-Cordova Census Area': 'Valdez-Cordova',
+    'Yakutat City and Borough': 'Yakutat',
+    'Yukon-Koyukuk Census Area': 'Yukon-Koyukuk'
+}
+
+regions['CountyName'] = regions['CountyName'].apply(
+    lambda x: county_dict[x] if x in county_dict.keys() else x
+    )
+
+regions['CountyName'] = regions['CountyName'].apply(lambda x: x if 'Parish' not in x else ' '.join(x.split()[:-1]))
+regions.at[2944, 'CountyName'] = 'Richmond City'
+regions.at[1597, 'CountyName'] = 'St. Louis City'
+
+regions['CountyName'] = regions['CountyName'].apply(lambda x: x if 'County' not in x else ' '.join(x.split()[:-1]))
+
+regions['county_state'] = regions['CountyName'] + regions['State']
+
+print(us_birds.shape)
+print(regions.shape)
+# Merge dataframes
+merged = us_birds.merge(regions)
+print(merged.shape)
