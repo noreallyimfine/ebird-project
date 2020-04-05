9a2d9f6e736ef2718ca1c94265898eef44fe658f
Avraham Jacobsohn <noreallyimfine@users.noreply.github.com>
2020-04-05 13:42:58 -0400

    complete read in 50000 new rows

--- sample/model_test.py
+++ sample/model_test.py
@@ -3,4 +3,12 @@ import joblib
 
 
 # Get 50,000 new instances from dataset
-df = pd.read_csv("C:\\Users\\ajaco")
+df = pd.read_csv("C:\\Users\\ajaco\\Desktop\\repos\\noreallyimfine\\ebird-project\\data\\ebd_relJan-2020.txt", 
+                  skiprows=200000,
+                  nrows=50000,
+                  usecols=['COMMON NAME', 'COUNTRY', 'STATE', 'COUNTY',
+                           'LATITUDE', 'LONGITUDE', 'OBSERVATION DATE',
+                           'OBSERVATION COUNT'])
+
+assert(df.shape == (50000, 8))
+
