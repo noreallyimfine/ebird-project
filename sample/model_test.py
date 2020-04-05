8ffec4cfeff8d83ca062906abffe37c802d39b24
Avraham Jacobsohn <noreallyimfine@users.noreply.github.com>
2020-04-05 14:14:37 -0400

    copy df to squelch setting with copy warning and drop bad names

--- sample/model_test.py
+++ sample/model_test.py
@@ -13,6 +13,7 @@ df = pd.read_csv("C:\\Users\\ajaco\\Desktop\\repos\\noreallyimfine\\ebird-projec
 
 assert(df.shape == (50000, 8))
 
+
 # Renaming columns
 df.rename(columns={
     'COMMON NAME': 'name',
@@ -33,5 +34,16 @@ us_birds = df.query("country == 'United States'")
 assert(us_birds.shape == (28548, 8))
 assert(len(us_birds['country'].unique()) == 1)
 
+# Copying dataframe
+us_birds = us_birds.copy()
+
 # Drop columns where county is missing
-print(us_birds.isnull().sum()['county'])
+assert(us_birds.isnull().sum()['county'] == 0)
+
+# Check for birds with names that we dropped for training
+us_birds['bad_name'] = us_birds['name'].apply(lambda x: 0 if ("sp." in x) or ("(" in x) or ("/" in x) else 1)
+
+mask = us_birds['bad_name'] == 0
+us_birds = us_birds[~mask].drop(columns=['bad_name'])
+assert(us_birds.shape == (28373, 8))
+
