41db75ac3be091b08a27bfa57ae621fbf5fe3c16
Avraham Jacobsohn <noreallyimfine@users.noreply.github.com>
2020-04-09 22:10:36 -0400

    add check for correct features

--- app/app.py
+++ app/app.py
@@ -18,17 +18,25 @@ def home():
     print("Method:", request.method)
     if request.method == 'GET':
         # TODO: Display dropdowns to choose bird, season, and region
-        # STRETCH: Let them input county and state and the app will find the county
+        # STRETCH: Let them input county and state and the app will find the region
         return "You are home"
     elif request.method == 'POST':
         # get values from post
         data = request.get_json()
-        print(data)
+        print("Data", data)
         # Pass to predict function
+        req_values = ['name', 'season', 'region']
+        # Check all required values are in the data
+        # If not, return error
+        if not all(value in data for value in req_values):
+            message = """Error: Missing required features. 
+                         Needs ['name', 'season', 'region']"""
+            return message, 400
         pred = predict(data)
         # return redirect to results route
         return "Thank you for posting"
     # return "You are home"
+
 
 @app.route('/results', methods=['GET'])
 def results():
@@ -36,6 +44,7 @@ def results():
     # Display image of bird
     # Display map of that birds prevalence
     pass
+
 
 if __name__ == "__main__":
     app.run(debug=True)
