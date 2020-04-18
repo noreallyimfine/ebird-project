b5413c346e4724816af5a6bdc84d4289bdd6f6e4
Avraham Jacobsohn <noreallyimfine@users.noreply.github.com>
2020-04-18 10:48:53 -0400

    move loading of pickled files to 'util' for neatness

--- bird_app/routes.py
+++ bird_app/routes.py
@@ -2,23 +2,10 @@ import pandas as pd
 from flask import Flask, request, jsonify, render_template
 from joblib import load
 from bird_app.forms import SightingForm
+from bird_app.util import encoder, model, birds, seasons, regions
 
 app = Flask(__name__)
 
-# Path here is relative, may get messed up in future
-encoder = load("bird_app/utils/cat_boost.joblib")
-model = load("bird_app/utils/rf.joblib")
-
-# Load birds, seasons, and regions
-birds_list = load('bird_app/utils/birds_list.joblib')
-seasons_list = load('bird_app/utils/seasons_list.joblib')
-regions_list = load('bird_app/utils/regions_list.joblib')
-
-# convert list to dict for displaying through flask template
-# (Hoping this isn't necessary for displaying all the birds)
-birds = [{'bird': bird} for bird in birds_list]
-seasons = [{'season': season} for season in seasons_list]
-regions = [{'region': region} for region in regions_list]
 
 # Dict to map results from model to english
 labels = {0: "Common", 1: "Uncommon", 2: "Rare"}
@@ -56,11 +43,11 @@ def home():
 
     elif request.method == 'POST':
         # get values from post
-        print(dir(request))
-        data = request.json
-        print(request.args)
-        print(request.form_data_parser_class())
-        print("Data", data)
+        # print(dir(request))
+        # data = request.json
+        # print(request.args)
+        # print(request.form_data_parser_class())
+        # print("Data", data)
 
         # Check all required values are in the data
         # If not, return error
