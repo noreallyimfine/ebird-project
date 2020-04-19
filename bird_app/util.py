cf6d15bc3bb7f101024f0f0dc651ef6db5db2c77
Avraham Jacobsohn <noreallyimfine@users.noreply.github.com>
2020-04-18 20:24:36 -0400

    split counties and states up - going to want to set it up so the form only shows counties from that state

--- bird_app/util.py
+++ bird_app/util.py
@@ -1,13 +1,13 @@
 from joblib import load
 
 # Path here is relative, may get messed up in future
-encoder = load("/utils/cat_boost.joblib")
-model = load("/utils/rf.joblib")
+encoder = load("bird_app/utils/cat_boost.joblib")
+model = load("bird_app/utils/rf.joblib")
 
 # Load birds, seasons, and regions
-birds_list = load('/utils/birds_list.joblib')
-seasons_list = load('/utils/seasons_list.joblib')
-regions_list = load('/utils/regions_list.joblib')
+birds_list = load('bird_app/utils/birds_list.joblib')
+seasons_list = load('bird_app/utils/seasons_list.joblib')
+regions_list = load('bird_app/utils/regions_list.joblib')
 
 # convert list to dict for displaying through flask template
 # (Hoping this isn't necessary for displaying all the birds)
@@ -17,4 +17,6 @@ regions = [{'region': region} for region in regions_list]
 
 # TODO: load county_state list and parse to separate for form
 # TODO: create dict to map countystate combo to region
-county_state_list = load('utils/county_state.joblib')
+county_state_list = load('bird_app/utils/county_state.joblib')
+counties = [x.split(',')[0] for x in county_state_list]
+states = [x.split(',')[1] for x in county_state_list]
