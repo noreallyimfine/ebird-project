c70e676d9b090a1d7f080812c6dbad717207e71d
Avraham Jacobsohn <noreallyimfine@users.noreply.github.com>
2020-04-18 21:15:48 -0400

    take out choices to see if it will work like that, but need CSRF token

--- bird_app/forms.py
+++ bird_app/forms.py
@@ -1,16 +1,14 @@
 from flask_wtf import FlaskForm
 from wtforms import SelectField, SubmitField
 from wtforms.validators import DataRequired
-from bird_app.util import birds, seasons, states, counties
 
 
 class SightingForm(FlaskForm):
     bird = SelectField("Which bird did you see?", 
-                       choices = [bird[bird] for bird in birds], 
                        validators=[DataRequired()])
     season = SelectField("Which season was it?",
-                         choices = [season[season] for season in seasons], 
                          validators=[DataRequired()])
-    state = SelectField("Which state were you in?", validators=[DataRequired()])
+    state = SelectField("Which state were you in?", 
+                        validators=[DataRequired()])
     county = SelectField("Which county were you in?", validators=[DataRequired()])
     submit = SubmitField("Check Rarity!")
