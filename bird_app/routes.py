3e3fe25c1a5751e59ca972f320dac681edb9e409
Avraham Jacobsohn <noreallyimfine@users.noreply.github.com>
2020-04-21 16:58:07 -0400

    removed predict func and its supporters and import it from predict module

--- bird_app/routes.py
+++ bird_app/routes.py
@@ -1,34 +1,11 @@
-import pandas as pd
-from flask import request, jsonify, render_template
+from flask import render_template
 from bird_app import app
 from bird_app.forms import SightingForm
-from bird_app.util import encoder, model, birds, seasons, regions
+from bird_app.predict import rare_pred
+
 
 # Dict to map results from model to english
 labels = {0: "Common", 1: "Uncommon", 2: "Rare"}
-
-
-def predict(form):
-    # parse data
-    bird = form.bird.data
-    season = form.season.data
-    state = form.state.data
-    county = form.county.data
-    
-    # TODO: Map State,county to region
-    region = county + state
-    # encode features
-    X = pd.DataFrame({
-        'name': [bird],
-        'season': [season],
-        'RegionName': [region]
-    })
-    print(X.shape)
-    X_encoded = encoder.transform(X)
-    # get prediction
-    pred = model.predict(X_encoded)
-    print("Prediction:", pred)
-    return pred[0]
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -36,7 +13,7 @@ def home():
     form = SightingForm()
     if form.validate_on_submit():
         # Pass to predict function
-        pred = predict(form)
+        pred = rare_pred(form)
         label = labels[pred]
         msg = {'prediction': label}
         # TODO: return redirect to results route
