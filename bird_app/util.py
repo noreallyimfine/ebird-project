e6bc1a1a996fb81539d985b1288894c35760de39
Avraham Jacobsohn <noreallyimfine@users.noreply.github.com>
2020-04-21 16:27:07 -0400

    remove dictionaries that are unnecessary, just need lists

--- bird_app/util.py
+++ bird_app/util.py
@@ -5,15 +5,15 @@ encoder = load("bird_app/utils/cat_boost.joblib")
 model = load("bird_app/utils/rf.joblib")
 
 # Load birds, seasons, and regions
-birds_list = load('bird_app/utils/birds_list.joblib')
-seasons_list = load('bird_app/utils/seasons_list.joblib')
-regions_list = load('bird_app/utils/regions_list.joblib')
+birds = load('bird_app/utils/birds_list.joblib')
+seasons = load('bird_app/utils/seasons_list.joblib')
+regions = load('bird_app/utils/regions_list.joblib')
 
 # convert list to dict for displaying through flask template
 # (Hoping this isn't necessary for displaying all the birds)
-birds = [{'bird': bird} for bird in birds_list]
-seasons = [{'season': season} for season in seasons_list]
-regions = [{'region': region} for region in regions_list]
+# birds = [{'bird': bird} for bird in birds_list]
+# seasons = [{'season': season} for season in seasons_list]
+# regions = [{'region': region} for region in regions_list]
 
 # TODO: load county_state list and parse to separate for form
 # TODO: create dict to map countystate combo to region
