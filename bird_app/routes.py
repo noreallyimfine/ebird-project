c17571fcd333557b24c2dd4626d41106c6110bd8
Avraham Jacobsohn <noreallyimfine@users.noreply.github.com>
2020-04-22 15:28:49 -0400

    change way im receiving post request and now it actually responds but still breaking

--- bird_app/routes.py
+++ bird_app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for
+from flask import render_template, request, redirect, url_for
 from bird_app import app
 from bird_app.forms import SightingForm
 from bird_app.predict import rare_pred
@@ -11,7 +11,7 @@ labels = {0: "Common", 1: "Uncommon", 2: "Rare"}
 @app.route('/', methods=['GET', 'POST'])
 def home():
     form = SightingForm()
-    if form.validate_on_submit():
+    if request.method == 'POST':
         # Pass to predict function
         pred = rare_pred(form)
         label = labels[pred]
