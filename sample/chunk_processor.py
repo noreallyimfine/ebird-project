508e24d06a1509b44991bf2927a2d93a565eb124
noreallyimfine <avrahamjacobsohn@gmail.com>
2020-05-15 11:16:49 -0400

    fill out region cleaning func

--- sample/chunk_processor.py
+++ sample/chunk_processor.py
@@ -2,3 +2,25 @@
 Using Pandas to process the entire bird file by using chunks and not reading 
 it all into memory at once. Then writes it to a new file.
 '''
+
+
+# Master region cleaning function
+def clean_regions(df):
+
+    # Copy df
+    df = df.copy()
+
+    # rename columns
+    df = region_column_renamer(df)
+
+    # clean state, county, and region cols
+    df = clean_cols(df)
+
+    # fix county names
+    df = county_fixed(df)
+
+    # combine county state
+    df = county_state_merger(df)
+
+    return df
+
