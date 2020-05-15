d74f0763de3b6c81e6c19d0456d3ad168aee1516
noreallyimfine <avrahamjacobsohn@gmail.com>
2020-05-15 11:21:32 -0400

    fill out master bird chunk cleaner

--- sample/chunk_processor.py
+++ sample/chunk_processor.py
@@ -24,3 +24,28 @@ def clean_regions(df):
 
     return df
 
+# Master bird chunk cleaner
+def clean_bird_chunks(df):
+
+    # copy df
+    df = df.copy()
+
+    # rename columns
+    df = bird_column_renamer(df)
+
+    # filter for us birds
+    df = us_bird_filter(df)
+
+    # drop nulls
+    df = df.dropna(subset=['county'])
+
+    # clean badly named birds
+    df = bad_name_cleaner(df)
+
+    # get season thru date
+    df = get_season(df)
+
+    # combine county state
+    df = county_state_merger(df)
+
+    return df 
