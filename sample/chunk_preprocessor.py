1a33b62ddab601e2a580fa898284a78820c892b7
noreallyimfine <avrahamjacobsohn@gmail.com>
2020-05-19 18:02:10 -0400

    read in regions and run through cleaner

--- sample/chunk_preprocessor.py
+++ sample/chunk_preprocessor.py
@@ -4,3 +4,12 @@ it all into memory at once. Then write it to a new file.
 '''
 
 from helper import clean_regions, clean_bird_chunks
+
+import pandas as pd
+
+regions = pd.read_excel("..//Bird-Check/Back-End/URAmericaMapCountyList.xlsx",
+                        skiprows=3,
+                        usecols=['State', 'CountyName', 'RegionName'])
+
+
+regions = clean_regions(regions)
