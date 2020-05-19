772d0be0fe38059c5a31e69b958ddfaf76080b92
noreallyimfine <avrahamjacobsohn@gmail.com>
2020-05-19 18:41:13 -0400

    working towards working lol

--- sample/chunk_preprocessor.py
+++ sample/chunk_preprocessor.py
@@ -6,15 +6,15 @@ it all into memory at once. Then write it to a new file.
 from helper import clean_regions, clean_bird_chunks
 
 import pandas as pd
+import sys; print(sys.path)
 
-regions = pd.read_excel("..//Bird-Check/Back-End/URAmericaMapCountyList.xlsx",
+regions = pd.read_excel("../../../Bird-Check/Back-End/URAmericaMapCountyList.xlsx",
                         skiprows=3,
                         usecols=['State', 'CountyName', 'RegionName'])
 
 
 regions = clean_regions(regions)
-
-bird_chunks = pd.read_csv("..//Bird-Check/bird_data.csv",
+bird_chunks = pd.read_csv(".././../Bird-Check/bird_data.csv",
                           sep='\t',
                           chunksize=500000,
                           usecols=['COMMON NAME', 'COUNTRY', 'STATE', 'COUNTY',
