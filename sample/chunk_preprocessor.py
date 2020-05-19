f9b602fba13e541a21412f595ee5cbc3febc572a
noreallyimfine <avrahamjacobsohn@gmail.com>
2020-05-19 17:42:49 -0400

    add docstring to chunk_preprocessor

--- sample/chunk_preprocessor.py
+++ sample/chunk_preprocessor.py
@@ -1,1 +1,6 @@
+'''
+Using Pandas to process the entire bird file by using chunks and not reading 
+it all into memory at once. Then write it to a new file.
+'''
+
 from helper import clean_regions, clean_bird_chunks
