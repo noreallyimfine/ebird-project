b2e8bda07bb5d0886967cffb7178d8dada1d4ec2
noreallyimfine <avrahamjacobsohn@gmail.com>
2020-07-29 17:48:17 -0400

    load birds seasons regions and county_states

--- bird_app/util.py
+++ bird_app/util.py
@@ -9,9 +9,9 @@ regions = load('bird_app/utils/regions_list.joblib')
 
 # convert list to dict for displaying through flask template
 # (Hoping this isn't necessary for displaying all the birds)
-# birds = [{'bird': bird} for bird in birds_list]
-# seasons = [{'season': season} for season in seasons_list]
-# regions = [{'region': region} for region in regions_list]
+birds = load('bird_app/utils/birds_list.joblib')
+seasons = load('bird_app/utils/seasons_list.joblib')
+regions = load('bird_app/utils/regions_list.joblib')
 
 # TODO: load county_state list and parse to separate for form
 # TODO: create dict to map countystate combo to region
