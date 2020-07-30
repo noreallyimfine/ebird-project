daaa5e2aeb9035052424532f00bb2feda81f2049
noreallyimfine <avrahamjacobsohn@gmail.com>
2020-07-29 20:36:48 -0400

    use secret key

--- bird_app/__init__.py
+++ bird_app/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
+from os import environ
 # from decouple import config
 
 app = Flask(__name__)
-# app.config['SECRET_KEY'] = config('SECRET_KEY')
+app.config['SECRET_KEY'] = environ.get('SECRET_KEY')
 
 from bird_app import routes
