eb4b1df6edbabffe8c525755c952457c9a694191
noreallyimfine <avrahamjacobsohn@gmail.com>
2020-07-30 11:20:20 -0400

    set the lookup table working i think

--- bird_app/lookup.py
+++ bird_app/lookup.py
@@ -2,6 +2,15 @@ import pandas as pd
 from bird_app.util import cs_to_region, lookup
 
 
+# def season_region_bird_rarity(bird, region, season):
+#     bird_percent = season_region_ct[(region, season)][bird] / season_region_ct[(region, season)].sum()
+#     if bird_percent > 0.005:
+#         return "Common"
+#     elif bird_percent > 0.001:
+#         return "Uncommon"
+#     else:
+#         return "Rare"
+
 def rareness_lookup(form):
     # parse data
     bird = form.bird.data
@@ -13,6 +22,15 @@ def rareness_lookup(form):
     cs = f'{county},{state}'
     region = cs_to_region[cs]
 
+    # lookup value in table as percent of total
+    bird_percent = lookup[(region, season)][bird] / lookup[(region, season)].sum()
+    if bird_percent > 0.005:
+        return "Common"
+    elif bird_percent > 0.001:
+        return "Uncommon"
+    else:
+        return "Rare"
+
     # encode features
-    message = f"You saw {bird} in {county},{state} in {season}"
+    message = f"You saw {bird} in {county},{state} in {season}, and the percent is {bird_percent}"
     return message
