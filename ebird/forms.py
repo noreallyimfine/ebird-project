0b2c086d0bd1b8e23a07915e809563f6579ed43d
noreallyimfine <avrahamjacobsohn@gmail.com>
2020-08-12 14:15:33 -0400

    add state to form

--- ebird/forms.py
+++ ebird/forms.py
@@ -1,12 +1,18 @@
 from flask_wtf import FlaskForm
 from wtforms import SelectField, SubmitField
 from wtforms.validators import DataRequired
-from ebird.models import Bird
+from ebird.models import Bird, State, County
 
 birds = Bird.query.all()
+states = State.query.all()
+counties = County.query.all()
+
 
 class RareForm(FlaskForm):
-    bird = SelectField('Which bird did you see?',
+    bird = SelectField('Choose a Bird',
                        validators=[DataRequired()],
                        choices=[(b.name, b.name) for b in birds])
+    state = SelectField('Choose a State',
+                       validators=[DataRequired()],
+                       choices=[(s.name, s.name) for s in states])
     submit = SubmitField('How rare?')
