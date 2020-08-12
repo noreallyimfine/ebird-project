5e4fb713e71b614d15e75b59de5cca816bbcefb8
noreallyimfine <avrahamjacobsohn@gmail.com>
2020-08-12 09:34:31 -0400

    add state and county tables

--- ebird/models.py
+++ ebird/models.py
@@ -3,4 +3,14 @@ from ebird import db
 
 class Bird(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(40))
+    name = db.Column(db.String(40), nullable=False)
+
+class State(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(25), nullable=False)
+
+
+class County(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    state = db.Column(db.String(25), db.ForeignKey('state.name'), nullable=False)
+    county_name = db.Column(db.String(50), nullable=False)
