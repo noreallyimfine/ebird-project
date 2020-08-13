c0608a157a796b1d93d35970e7bec567ca548d5f
noreallyimfine <avrahamjacobsohn@gmail.com>
2020-08-13 06:49:43 -0400

    add region and season models

--- ebird/models.py
+++ ebird/models.py
@@ -13,5 +13,16 @@ class State(db.Model):
 
 class County(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    state = db.Column(db.String(25), db.ForeignKey('state.name'), nullable=False)
+    state = db.Column(db.String(25), db.ForeignKey('state.id'), nullable=False)
     county_name = db.Column(db.String(50), nullable=False)
+    region = db.relationship('Region', backref='region_county', lazy=True)
+
+class Region(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    county_id = db.Column(db.String(50), db.ForeignKey('county.id'), nullable=False)
+    state = db.Column(db.String(25), nullable=False)
+    region = db.Column(db.String(20), nullable=False)
+
+class Season(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(10), nullable=False)
